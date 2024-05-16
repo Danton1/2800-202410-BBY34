ae4efe9651e5e5bfa577359b1e1d4e97afcb3072
tysonchan1 <tyson95577@gmail.com>
2024-05-16 10:22:27 -0700

    fixed signup page problem

--- scripts/signUpPage.js
+++ scripts/signUpPage.js
@@ -1,12 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const Joi = require("joi");
+const expireTime = 24 * 60 * 60 * 1000; //expires after 24 hr  (hours * minutes * seconds * millis)
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+var {database} = include('databaseConnection');
+const userCollection = database.db(mongodb_database).collection('users');
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+// USES
+
+router.use(express.urlencoded({extended: false}));
+router.use(express.static(__dirname + "/public"));
+
+router.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+
+
 router.get('/', (req, res) => {
     res.render("signUpPage");
 });
 
 
 router.post('/submitSignUp', async (req,res) => {
+    console.log(req.body.firstName);
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var birthDate = req.body.birthDate;
