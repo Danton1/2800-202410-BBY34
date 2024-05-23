63e8acf34d8cd06ff17f22b5d9b959d3e4484f4d
jjhatu <j.jhatu1@gmail.com>
2024-05-23 08:19:30 -0700

    Connected database to personalInfo.ejs and implemented edit fun ction

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("./utils.js");
-require('dotenv').config();
+require('dotenv').config(); 
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
-
+const { database } = require('./databaseConnection');
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -32,199 +33,305 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-var {database} = include('databaseConnection');
+//connect to database
+async function connectToDatabase() {
+    const uri = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`; // Updated URI
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const userCollection = database.db(mongodb_database).collection('users');
-const tokenCollection = database.db(mongodb_database).collection('forgotToken');
-
-app.set('view engine', 'ejs');
-
-var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
-})
-
-// USES
-
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(__dirname + "/public"));
-
-app.use(session({ 
-    secret: node_session_secret,
-	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
-	resave: true
-}
-));
-
-// FUNCTIONS
-
-function isValidSession(req) {
-    if (req.session.authenticated) {
-        return true;
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+        return client;
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+        throw err;
     }
-    return false;
 }
 
-function sessionValidation(req,res,next) {
-    if (isValidSession(req)) {
-        next();
+(async () => {
+    const client = await connectToDatabase();
+    const database = client.db(mongodb_database);
+
+    const userCollection = database.collection('users');
+    const tokenCollection = database.collection('forgotToken');
+
+    app.set('view engine', 'ejs');
+
+    var mongoStore = MongoStore.create({
+        mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+        crypto: {
+            secret: mongodb_session_secret
+        }
+    });
+
+    // USES
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.static(__dirname + "/public"));
+
+    app.use(session({
+        secret: node_session_secret,
+        store: mongoStore, //default is memory store 
+        saveUninitialized: false,
+        resave: true
+    }));
+
+    // FUNCTIONS
+
+    function isValidSession(req) {
+        if (req.session.authenticated) {
+            return true;
+        }
+        return false;
     }
-    else {
+
+    function sessionValidation(req, res, next) {
+        if (isValidSession(req)) {
+            next();
+        } else {
+            res.redirect('/login');
+        }
+    }
+
+    function isAdmin(req) {
+        if (req.session.user_type == 'admin') {
+            return true;
+        }
+        return false;
+    }
+
+    function adminAuthorization(req, res, next) {
+        console.log(req.session.user_type);
+        if (!isAdmin(req)) {
+            res.status(403);
+            res.render("403");
+            return;
+        } else {
+            next();
+        }
+    }
+
+    // GETS
+
+    app.get('/', (req, res) => {
+        if (isValidSession(req)) {
+            res.render('index', { username: req.session.firstName, openWeatherAPIKey: process.env.OPEN_WEATHER_API_KEY });
+            return;
+        }
         res.redirect('/login');
-    }
-}
+    });
 
-function isAdmin(req) {
-    if (req.session.user_type == 'admin') {
-        return true;
-    }
-    return false;
-}
+    // Get for login
+    app.get('/login', (req, res) => {
+        res.render("loginPage");
+    });
 
-function adminAuthorization(req, res, next) {
-	console.log(req.session.user_type);
-	if (!isAdmin(req)) {
-        res.status(403);
-        res.render("403");
-        return;
-    }
-    else {
-        next();
-    }
-}
-
-// GETS
-
-app.get('/', (req,res) => {
-    if(isValidSession(req)){
-        res.render('index', {username: req.session.firstName, openWeatherAPIKey: process.env.OPEN_WEATHER_API_KEY});
-        return;
-    }
-    res.redirect('/login');
-});
-
-// Get for login
-app.get('/login', (req, res) => {
-    res.render("loginPage");
-});
-
-// Get for profile
-app.get('/profile', (req,res) => {
-    if(isValidSession(req)){
-        res.render('profilePage', {user: req.session});
-        return;
-    }
-    res.redirect('/login');
-});
-app.get('/profile/personalInfo', (req,res) => {
-    if(isValidSession(req)){
-        res.render('personalInfoPage', {user: req.session});
-        return;
-    }
-    res.redirect('/login');
-});
-app.get('/profile/contactInfo', (req,res) => {
-    if(isValidSession(req)){
-        res.render('contactInfoPage', {user: req.session});
-        return;
-    }
-    res.redirect('/login');
-});
-app.get('/profile/medHistory', (req,res) => {
-    if(isValidSession(req)){
-        res.render('medicalHistoryPage', {user: req.session});
-        return;
-    }
-    res.redirect('/login');
-});
-
-// Get for Settings
-app.get('/settings', (req,res) => {
-    res.render('settingsPage');
-});
-app.get('/settings/signOut', (req, res) => {
-    req.session.destroy();
-    console.log("You are now logged out.");
-    res.redirect("/login");
-});
-
-// Get for chatbot
-app.get('/chatbot', (req, res) => {
-    res.render("chatbotPage");
-});
-
-// Get for 404
-app.get("*", (req,res) => {
-	res.status(404);
-	res.render("404Page");
-})
-
-// POSTS
-
-app.post('/editPass', async(req,res) => {
-    res.redirect('/getPassEdit');
-});
-
-app.post('/submitLogin', async (req,res) => {
-    var email = req.body.loginPageEmailInput;
-    var password = req.body.loginPagePasswordInput;
-
-	const schema = Joi.object({
-        email: Joi.string().max(20).required(),
-        password: Joi.string().max(20).required()
-	});
-
-	const validationResult = schema.validate({email, password});
-	if (validationResult.error != null) {
-        res.render("errorPage", {error: validationResult.error});
-        return;
-	}
-
-	const result = await userCollection.find({email: email}).project({email: 1, password: 1, _id: 1}).toArray();
-
-    // Getting the userName info from the email
-	let getUser = userCollection.findOne({email: email}).then((user) => {
-        if (!user) {
-            //if user does not exist, the authentication failed
-			res.render("errorPage", {prompt: "Invalid email account"});
+    // Get for profile
+    app.get('/profile', (req, res) => {
+        if (isValidSession(req)) {
+            res.render('profilePage', { user: req.session });
             return;
         }
-        //assign the user to getUser variable
-        getUser = user;
-    })
-
-	if (result.length == 0) {
-        res.render("errorPage", {error: "No user with that email found"});
-        return;
-	} else if (result.length != 1) {
-        res.render("errorPage", {error: "More than one user found."});
-        return;
-    } else {
-        if (await bcrypt.compare(password, result[0].password)) {
-            req.session.authenticated = true;
-            req.session.cookie.maxAge = expireTime;
-            req.session.email = result[0].email;
-            req.session.firstName = getUser.firstName;
-            req.session.lastName = getUser.lastName;
-            req.session.birthDate = getUser.birthDate;
-            req.session.country = getUser.country;
-            req.session.city = getUser.city;
-
-            res.redirect('/');
+        res.redirect('/login');
+    });
+    // Get for personal Info 
+    app.get('/profile/personalInfo', async (req, res) => {
+        if (isValidSession(req)) {
+            try {
+                const user = await userCollection.findOne({ email: req.session.email });
+                res.render('personalInfoPage', { user });
+            } catch (err) {
+                console.error("Error fetching user data:", err);
+                res.status(500).send("Internal Server Error");
+            }
             return;
         }
-        else {
-            res.render("errorPage", {error: "Invalid password."});
+        res.redirect('/login');
+    });
+
+    // Get for Contact Info
+    app.get('/profile/contactInfo', (req,res) => {
+        if(isValidSession(req)){
+            res.render('contactInfoPage', {user: req.session});
             return;
         }
-    }	
-});
+        res.redirect('/login');
+    });
+    
+    // Get for medical History
+    app.get('/profile/medHistory', (req,res) => {
+        if(isValidSession(req)){
+            res.render('medicalHistoryPage', {user: req.session});
+            return;
+        }
+        res.redirect('/login');
+    });
+    
 
-// LISTENS
+    // Get for Settings
+    app.get('/settings', (req, res) => {
+        res.render('settingsPage');
+    });
+    app.get('/settings/signOut', (req, res) => {
+        req.session.destroy();
+        console.log("You are now logged out.");
+        res.redirect("/login");
+    });
 
-app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+    // Get for chatbot
+    app.get('/chatbot', (req, res) => {
+        res.render("chatbotPage");
+    });
+
+    // Get for 404
+    app.get("*", (req, res) => {
+        res.status(404);
+        res.render("404Page");
+    });
+
+    // POSTS
+
+    app.post('/editPass', async (req, res) => {
+        res.redirect('/getPassEdit');
+    });
+
+    app.post('/submitLogin', async (req, res) => {
+        var email = req.body.loginPageEmailInput;
+        var password = req.body.loginPagePasswordInput;
+    
+        const schema = Joi.object({
+            email: Joi.string().max(20).required(),
+            password: Joi.string().max(20).required()
+        });
+    
+        const validationResult = schema.validate({ email, password });
+        if (validationResult.error != null) {
+            res.render("errorPage", { error: validationResult.error });
+            return;
+        }
+    
+        const result = await userCollection.find({ email: email }).project({ email: 1, password: 1, _id: 1 }).toArray();
+    
+        // Getting the userName info from the email
+        let getUser = await userCollection.findOne({ email: email });
+        if (!getUser) {
+            res.render("errorPage", { prompt: "Invalid email account" });
+            return;
+        }
+    
+        if (result.length == 0) {
+            res.render("errorPage", { error: "No user with that email found" });
+            return;
+        } else if (result.length != 1) {
+            res.render("errorPage", { error: "More than one user found." });
+            return;
+        } else {
+            if (await bcrypt.compare(password, result[0].password)) {
+                req.session.authenticated = true;
+                req.session.cookie.maxAge = expireTime;
+                req.session.email = result[0].email;
+                req.session.firstName = getUser.firstName;
+                req.session.lastName = getUser.lastName;
+                req.session.birthDate = getUser.birthDate;
+                req.session.country = getUser.country;
+                req.session.city = getUser.city;
+    
+                res.redirect('/');
+                return;
+            } else {
+                res.render("errorPage", { error: "Invalid password." });
+                return;
+            }
+        }
+    });
+    
+    //post for Personal Info
+    app.post('/profile/personalInfo/edit', async (req, res) => {
+        if (isValidSession(req)) {
+            try {
+                const { firstName, lastName, birthDate, country, city } = req.body;
+                await userCollection.updateOne(
+                    { email: req.session.email },
+                    { $set: { firstName, lastName, birthDate, country, city } }
+                );
+                
+                req.session.firstName = firstName;
+                req.session.lastName = lastName;
+                req.session.birthDate = birthDate;
+                req.session.country = country;
+                req.session.city = city;
+                
+                res.redirect('/profile/personalInfo');
+            } catch (err) {
+                console.error("Error updating user data:", err);
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+    //post for Medical History (meds)
+    app.post('/profile/medHistory/addMedication', async (req, res) => {
+        if (isValidSession(req)) {
+            try {
+                const { medication } = req.body;
+                await userCollection.updateOne(
+                    { email: req.session.email },
+                    { $push: { medications: medication } }
+                );
+                res.redirect('/profile/medHistory');
+            } catch (err) {
+                console.error("Error adding medication:", err);
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+        //post for Medical History (illness)
+    app.post('/profile/medHistory/addIllness', async (req, res) => {
+        if (isValidSession(req)) {
+            try {
+                const { illness } = req.body;
+                await userCollection.updateOne(
+                    { email: req.session.email },
+                    { $push: { illnesses: illness } }
+                );
+                res.redirect('/profile/medHistory');
+            } catch (err) {
+                console.error("Error adding illness:", err);
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+        //post for Medical History (allergy)
+    app.post('/profile/medHistory/addAllergy', async (req, res) => {
+        if (isValidSession(req)) {
+            try {
+                const { allergy } = req.body;
+                await userCollection.updateOne(
+                    { email: req.session.email },
+                    { $push: { allergies: allergy } }
+                );
+                res.redirect('/profile/medHistory');
+            } catch (err) {
+                console.error("Error adding allergy:", err);
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+            res.redirect('/login');
+        }
+    });
+    
+
+    // LISTENS
+
+    app.listen(port, () => {
+        console.log("Node application listening on port " + port);
+    });
+})();
