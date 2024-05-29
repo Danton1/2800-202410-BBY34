10945dfa327f0f5d1c8b88a8aa4c5f92b3c59355
Soomin Jeong <sjeong0643@gmail.com>
2024-05-29 07:02:16 -0700

    Added a profile picture for Soomin and Danton on the About Us page

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,24 +5,8 @@ router.get('/account', (req,res) => {
     res.redirect('/profile');
 });
 
-// router.get('/language', (req,res) => {
-//     res.render("language");
-// });
-
 router.get('/aboutUs', (req,res) => {
     res.render("aboutUsPage");
 });
-
-// router.get('/terms', (req,res) => {
-//     res.render("terms");
-// });
-
-// router.get('/contact', (req,res) => {
-//     res.render("contact");
-// });
-
-// router.get('/faq', (req,res) => {
-//     res.render("faq");
-// });
 
 module.exports = router;
